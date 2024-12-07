b2d67876c72382a8dfdb9765561ee53de54e3b29
Gabriel Oliver <itsgabrieloliver@proton.me>
2024-12-07 12:36:24 -0500

    perf: use iterators and closures

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use basic_cli::Config;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|error| {
+    let config = Config::new(env::args()).unwrap_or_else(|error| {
         eprintln!("Failed to generate configuration: {}", error);
         process::exit(1);
     });
